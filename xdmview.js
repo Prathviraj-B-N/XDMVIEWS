1c999b09d2e1fab9345cb465fe501dc3dabe5552
Prathviraj B N <prathviraj.b.n@gmail.com>
2023-03-12 13:45:24 +0530

    xml to js

--- xdmview.js
+++ xdmview.js
@@ -1,67 +1,84 @@
-const fs = require('fs');
-const xml2js = require('xml2js');
-const mysql = require('mysql');
+const fs = require("fs");
+const xml2js = require("xml2js");
+const mysql = require("mysql");
 
-/**
- * Reads input from a JSON, XML, or RDBMS file and returns the data as an object.
- *
- * @param {string} input_file_path - The path to the input file.
- * @param {string} input_file_format - The format of the input file ('json', 'xml', or 'rdbms').
- * @param {Object} [rdbms_config] - The configuration options for connecting to an RDBMS database (optional).
- * @returns {Promise<Object>} - A Promise that resolves to the input data as an object.
- */
-function read_input(input_file_path, input_file_format, rdbms_config) {
-  return new Promise((resolve, reject) => {
-    if (input_file_format === 'json') {
-      fs.readFile(input_file_path, 'utf-8', (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          try {
-            const input_data = JSON.parse(data);
-            resolve(input_data);
-          } catch (parse_err) {
-            reject(parse_err);
-          }
-        }
-      });
-    } else if (input_file_format === 'xml') {
-      const parser = new xml2js.Parser();
+function init(xmlFilePath) {
+  // Read the XML file
+  const xmlData = fs.readFileSync(xmlFilePath, "utf-8");
 
-      fs.readFile(input_file_path, 'utf-8', (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          parser.parseString(data, (parse_err, result) => {
-            if (parse_err) {
-              reject(parse_err);
-            } else {
-              resolve(result);
-            }
-          });
-        }
-      });
-    } else if (input_file_format === 'rdbms') {
-      const connection = mysql.createConnection(rdbms_config);
-
-      connection.connect((connect_err) => {
-        if (connect_err) {
-          reject(connect_err);
-        } else {
-          const query = 'SELECT * FROM ' + rdbms_config.table_name;
-          connection.query(query, (query_err, rows, fields) => {
-            if (query_err) {
-              reject(query_err);
-            } else {
-              resolve(rows);
-            }
-          });
-        }
-      });
-    } else {
-      reject(new Error('Invalid input file format.'));
+  // Parse the XML data to a JavaScript object
+  const parser = new xml2js.Parser();
+  parser.parseString(xmlData, (err, result) => {
+    if (err) {
+      console.error("Failed to parse the XML data:", err);
+      return;
     }
+
+    // Extract the sources and their configurations
+    const sources = {};
+    result.xdmview.source.forEach((source) => {
+      const type = source.$.type;
+      const config = {
+        url: source.$.url,
+        name: source.$.name,
+      };
+
+      // Add type-specific configurations
+      if (type === "mysql") {
+        config.username = source.$.username;
+        config.password = source.$.password;
+      }
+
+      sources[config.name] = {
+        type,
+        config,
+      };
+    });
+
+    // Create the views based on the specified bases and parents
+    result.xdmview.base?.forEach((base) => {
+      const sourceName = base.$.source;
+      const source = sources[sourceName];
+      const baseName = base.$.name;
+      const fields = base.field.map((field) => ({
+        name: field._,
+        conditions: field.$.conditions,
+        selected: field.$.selected === "true",
+      }));
+
+      // TODO: Create the base view using the specified source and fields
+      console.log(
+        `Creating base view '${baseName}' using ${source.type} source '${sourceName}' with fields:`,
+        fields
+      );
+    });
   });
 }
 
-module.exports = { read_input };
+
+function connectMySQL() {
+
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+  });
+
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected!");
+
+    connection.query(
+      "CREATE VIEW myview AS SELECT * FROM db1.table1 JOIN db2.table2 ON db1.table1.id = db2.table2.id",
+      (err, result) => {
+        if (err) throw err;
+        console.log("View created!");
+        console.log(result);
+      }
+    );
+  });
+}
+
+// Example usage:
+init("./inputXml.xml");
+// connectMySQL();
